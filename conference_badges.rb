2fab0371d6f80c6f734c560edcb8d9db3b471ea0
Elijah wines <elijahwines84@yahoo.com>
2020-02-18 15:36:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+  attendees.each do |name|
+     puts "Hello, my name is #{name}."
+
+  end
+end
